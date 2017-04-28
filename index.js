2508274829b824b750d8f825e106a0165b21ed16
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-28 20:25:57 +0700

    code show ejs

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('index_dark'));
+app.get('/', (req, res) => res.render('index_dark', { mang: arrProducts }));
 
 class Product {
-
+    constructor(name, desc, image, idVideo) {
+        this.name = name;
+        this.desc = desc;
+        this.image = image;
+        this.idVideo = idVideo;
+    }
 }
 
-const arrProducts = [];
+const arrProducts = [
+    new Product('React Native', 'It is a Facebook javascript lib!', '16500156_th.jpg', '213831476'),
+    new Product('NodeJS', 'Develop by Rald, using V8 engine of chrome', '18058390_th.jpg', '18058390')
+];
